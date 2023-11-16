2360ae109a4739f8edb50271b3c335375bbd2cd3
Vikash Patel <77643994+vikashpatel24@users.noreply.github.com>
2023-11-16 03:37:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember()
+{
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+
+    member.style.display = "block";
+    skills.style.display = "none";
+}
